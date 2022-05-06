51e7696e5fd1b8f10d248ce759df2ccda2e04852
Steven Peng <steven020211@gmail.com>
2022-05-07 07:41:44 +0800

    加入專案檔案。

---
+++ ParkingPractice/Solution.cs
@@ -0,0 +1,110 @@
+﻿public class ParkingFee
+{
+    public IEnumerable<SingleDayFee> Items { get; private set; }
+    public int TotalFee => Items.Sum(item => item.Fee);
+
+    public ParkingFee(IEnumerable<SingleDayFee> multipleDayFees)
+    {
+        Items = multipleDayFees;
+    }
+}
+
+// 單日停車資訊
+public class SingleDayFee
+{
+    public DateTime StartTime { get; set; } // 精確到分鐘的入場時間
+    public DateTime EndTime { get; set; } // 精確到分鐘的離場時間
+    public int Fee { get; set; } // 本日應收取費用
+
+    public SingleDayFee(DateTime start, DateTime end, Func<DateTime, DateTime, int> getFee)
+    {
+        StartTime = start;
+        EndTime = end;
+        Fee = getFee(start, end);
+    }
+}
+
+public class Solution
+{
+    #region Q3 計算跨日的停車費
+    public ParkingFee CalcParkingFee(DateTime start, DateTime end)
+    {
+        if (end < start) throw new ArgumentException();
+
+        start = start.SkipToMinutes();
+        end = end.SkipToMinutes();
+
+        List<SingleDayFee> fees = new List<SingleDayFee>();
+
+        var currentStart = start;
+        var currentEnd = start.DateEnd().CompareAndTakeSmaller(end);
+
+        //第一天
+        fees.Add(new SingleDayFee(currentStart, currentEnd, CalcFee));
+        void shiftNextDay()
+        {
+            currentStart = currentStart.NextDateStart();
+            currentEnd = currentStart.DateEnd();
+        };
+        //中間天數
+        for (shiftNextDay();
+            currentEnd < end;
+            shiftNextDay())
+            fees.Add(new SingleDayFee(currentStart, currentEnd, (_, _) => FEE_MAX));
+        //最後一日
+        if (currentStart < end)
+        {
+            currentEnd = currentEnd.CompareAndTakeSmaller(end);
+            fees.Add(new SingleDayFee(currentStart, currentEnd, CalcFee));
+        }
+
+        return new ParkingFee(fees);
+
+    }
+    #endregion
+
+    #region Q2 計算單日的停車費
+    //免費時段
+    private readonly TimeSpan FREE_TIME = TimeSpan.FromMinutes(10);
+    //最高收費
+    private const int FEE_MAX = 50;
+    //觀察題目規律, 每小時 10 元, 前半小時 7 元, 後半小時 3 元
+    private const int FEE_HOUR = 10;
+    private const int FEE_FIRST_HALF_HOUR = 7;
+
+    public int CalcFee(DateTime start, DateTime end)
+    {
+        //捨去到分
+        start = start.SkipToMinutes();
+        end = end.SkipToMinutes();
+
+        var stay = end - start;
+        //免費時段內不收費
+        if (stay <= FREE_TIME) return 0;
+
+        //總時數費用
+        var fee = (int)stay.TotalHours * FEE_HOUR;
+        //剩餘未滿一小時費用
+        if (stay.Minutes > 0) fee += (int)stay.Minutes <= 30 ? FEE_FIRST_HALF_HOUR : FEE_HOUR;
+        //檢查最高收費
+        return Math.Min(FEE_MAX, fee);
+    }
+    #endregion
+
+    #region Q1 計算停車分鐘數
+    public int GetStayMinutes(DateTime start, DateTime end) =>
+        (int)(end.SkipToMinutes() - start.SkipToMinutes()).TotalMinutes;
+    #endregion
+}
+
+public static class Extensions
+{
+    public static DateTime SkipToMinutes(this DateTime dateTime) =>
+        dateTime.Date.AddMinutes((int)dateTime.TimeOfDay.TotalMinutes);
+    public static DateTime NextDateStart(this DateTime dateTime) =>
+        dateTime.Date.AddDays(1);
+    public static DateTime DateEnd(this DateTime dateTime) =>
+        dateTime.NextDateStart().AddMinutes(-1);
+    public static DateTime CompareAndTakeSmaller(this DateTime dateTime, DateTime dateTimeToCompare) =>
+        dateTime < dateTimeToCompare ? dateTime : dateTimeToCompare;
+}
---
+++ ParkingPracticeTests/SolutionTests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        [DataRow("00:00:00", "23:59:59", 50, DisplayName = "全天\t50")]
+        [DataRow("09:00:00", "09:00:00", 0, DisplayName = "[0, 10] = 0")]
+        [DataRow("09:00:00", "09:10:59", 0, DisplayName = "[0, 10] = 0")]
+        [DataRow("09:00:00", "09:11:00", 7, DisplayName = "[11, 30] = 7")]
+        [DataRow("09:00:00", "09:30:59", 7, DisplayName = "[11, 30] = 7")]
+        [DataRow("09:00:00", "09:31:00", 10, DisplayName = "[31, 59] = 10")]
+        [DataRow("09:00:00", "09:59:59", 10, DisplayName = "[31, 59] = 10")]
+        [DataRow("09:00:00", "10:00:59", 10, DisplayName = "[60] = 10")]
+        [DataRow("09:00:00", "10:01:00", 17, DisplayName = "[61, 90] = 17")]
+        [DataRow("09:00:00", "10:30:59", 17, DisplayName = "[61, 90] = 17")]
+        [DataRow("09:00:00", "10:31:00", 20, DisplayName = "[91, 119] = 20")]
+        [DataRow("09:00:00", "10:59:59", 20, DisplayName = "[91, 119] = 20")]
+        [DataRow("09:00:00", "11:00:59", 20, DisplayName = "[120] = 20")]
+        [DataRow("09:00:00", "11:01:00", 27, DisplayName = "[121, 150] = 27")]
+        [DataRow("09:00:00", "11:30:59", 27, DisplayName = "[121, 150] = 27")]
+        [DataRow("09:00:00", "11:31:00", 30, DisplayName = "[151, 179] = 30")]
+        [DataRow("09:00:00", "11:59:59", 30, DisplayName = "[151, 179] = 30")]
+        [DataRow("09:00:00", "12:00:59", 30, DisplayName = "[180] = 30")]
+        [DataRow("09:00:00", "12:01:00", 37, DisplayName = "[181, 210] = 37")]
+        [DataRow("09:00:00", "12:30:59", 37, DisplayName = "[181, 210] = 37")]
+        [DataRow("09:00:00", "12:31:00", 40, DisplayName = "[211, 239] = 40")]
+        [DataRow("09:00:00", "12:59:59", 40, DisplayName = "[211, 239] = 40")]
+        [DataRow("09:00:00", "13:00:59", 40, DisplayName = "[240] = 40")]
+        [DataRow("09:00:00", "13:01:00", 47, DisplayName = "[241, 270] = 47")]
+        [DataRow("09:00:00", "13:30:59", 47, DisplayName = "[241, 270] = 47")]
+        [DataRow("09:00:00", "13:31:00", 50, DisplayName = "[271, 299] = 50")]
+        [DataRow("09:00:00", "13:59:59", 50, DisplayName = "[271, 299] = 50")]
+        [DataRow("09:00:00", "14:00:59", 50, DisplayName = "[300] = 50")]
+        [DataRow("09:00:00", "14:01:00", 50, DisplayName = "[301, 330] = 50")]
+        [DataRow("09:00:00", "14:30:59", 50, DisplayName = "[301, 330] = 50")]
+        [DataRow("09:00:00", "14:31:00", 50, DisplayName = "[331, 359] = 50")]
+        [DataRow("09:00:00", "14:59:59", 50, DisplayName = "[331, 359] = 50")]
+        [DataRow("09:00:00", "15:00:59", 50, DisplayName = "[360] = 50")]
+        public void CalcFeeTest(string start, string end, int expect)
+        {
+            Solution solution = new Solution();
+            int res = solution.CalcFee(DateTime.Parse(start), DateTime.Parse(end));
+            Assert.AreEqual(expect, res);
+        }
+
+        [TestMethod()]
+        [DataRow("00:00:00", "23:59:59", 50, DisplayName = "全天\t50")]
+        [DataRow("09:00:00", "09:00:00", 0, DisplayName = "[0, 10] = 0")]
+        [DataRow("09:00:00", "09:10:59", 0, DisplayName = "[0, 10] = 0")]
+        [DataRow("09:00:00", "09:11:00", 7, DisplayName = "[11, 30] = 7")]
+        [DataRow("09:00:00", "09:30:59", 7, DisplayName = "[11, 30] = 7")]
+        [DataRow("09:00:00", "09:31:00", 10, DisplayName = "[31, 59] = 10")]
+        [DataRow("09:00:00", "09:59:59", 10, DisplayName = "[31, 59] = 10")]
+        [DataRow("09:00:00", "10:00:59", 10, DisplayName = "[60] = 10")]
+        [DataRow("09:00:00", "10:01:00", 17, DisplayName = "[61, 90] = 17")]
+        [DataRow("09:00:00", "10:30:59", 17, DisplayName = "[61, 90] = 17")]
+        [DataRow("09:00:00", "10:31:00", 20, DisplayName = "[91, 119] = 20")]
+        [DataRow("09:00:00", "10:59:59", 20, DisplayName = "[91, 119] = 20")]
+        [DataRow("09:00:00", "11:00:59", 20, DisplayName = "[120] = 20")]
+        [DataRow("09:00:00", "11:01:00", 27, DisplayName = "[121, 150] = 27")]
+        [DataRow("09:00:00", "11:30:59", 27, DisplayName = "[121, 150] = 27")]
+        [DataRow("09:00:00", "11:31:00", 30, DisplayName = "[151, 179] = 30")]
+        [DataRow("09:00:00", "11:59:59", 30, DisplayName = "[151, 179] = 30")]
+        [DataRow("09:00:00", "12:00:59", 30, DisplayName = "[180] = 30")]
+        [DataRow("09:00:00", "12:01:00", 37, DisplayName = "[181, 210] = 37")]
+        [DataRow("09:00:00", "12:30:59", 37, DisplayName = "[181, 210] = 37")]
+        [DataRow("09:00:00", "12:31:00", 40, DisplayName = "[211, 239] = 40")]
+        [DataRow("09:00:00", "12:59:59", 40, DisplayName = "[211, 239] = 40")]
+        [DataRow("09:00:00", "13:00:59", 40, DisplayName = "[240] = 40")]
+        [DataRow("09:00:00", "13:01:00", 47, DisplayName = "[241, 270] = 47")]
+        [DataRow("09:00:00", "13:30:59", 47, DisplayName = "[241, 270] = 47")]
+        [DataRow("09:00:00", "13:31:00", 50, DisplayName = "[271, 299] = 50")]
+        [DataRow("09:00:00", "13:59:59", 50, DisplayName = "[271, 299] = 50")]
+        [DataRow("09:00:00", "14:00:59", 50, DisplayName = "[300] = 50")]
+        [DataRow("09:00:00", "14:01:00", 50, DisplayName = "[301, 330] = 50")]
+        [DataRow("09:00:00", "14:30:59", 50, DisplayName = "[301, 330] = 50")]
+        [DataRow("09:00:00", "14:31:00", 50, DisplayName = "[331, 359] = 50")]
+        [DataRow("09:00:00", "14:59:59", 50, DisplayName = "[331, 359] = 50")]
+        [DataRow("09:00:00", "15:00:59", 50, DisplayName = "[360] = 50")]
+        [DataRow("2002/5/1 23:50:00", "2002/5/2 00:10:59", 0, DisplayName = "跨1天,時間極短")]
+        [DataRow("2002/5/1 23:48:00", "2002/5/2 00:11:59", 14, DisplayName = "跨1天,必需付費")]
+        [DataRow("2002/5/1 23:48:00", "2002/5/3 00:11:59", 64, DisplayName = "跨2天,必需付費")]
+        public void CalcParkingFeeTest(string start, string end, int expect)
+        {
+            var parkingFee = new Solution().CalcParkingFee(DateTime.Parse(start), DateTime.Parse(end));
+            Assert.AreEqual(expect, parkingFee.TotalFee);
+        }
+
+        [TestMethod("start < end")]
+        public void CalcParkingFeeExceptionTest()
+        {
+            var start = DateTime.Parse("2002/5/1 23:50:00");
+            var end = DateTime.Parse("2002/5/1 00:10:59");
+
+            Assert.ThrowsException<ArgumentException>(() => new Solution().CalcParkingFee(start, end));
+        }
+    }
+}
