bd1195f16a7c7358a9fb69932e3d968b666653c3
Steven Peng <steven020211@gmail.com>
2022-05-12 10:27:33 +0800

    format

--- ParkingPractice/ParkingTime.cs
+++ ParkingPractice/ParkingTime.cs
@@ -18,10 +18,10 @@ namespace ParkingPractice
         public int CompareTo(DateTime other) => Value.CompareTo(other);
 
         #region 運算子多載
-        public override bool Equals([NotNullWhen(true)] object? obj)=> base.Equals(obj);
+        public override bool Equals([NotNullWhen(true)] object? obj) => base.Equals(obj);
         public override int GetHashCode() => base.GetHashCode();
 
-        public static bool operator ==(ParkingTime parkingTime1, ParkingTime parkingTime2)=>parkingTime1.Value == parkingTime2.Value;
+        public static bool operator ==(ParkingTime parkingTime1, ParkingTime parkingTime2) => parkingTime1.Value == parkingTime2.Value;
         public static bool operator !=(ParkingTime parkingTime1, ParkingTime parkingTime2) => !(parkingTime1 == parkingTime2);
 
         public static bool operator >(ParkingTime parkingTime1, ParkingTime parkingTime2) => parkingTime1.Value > parkingTime2.Value;
@@ -32,7 +32,7 @@ namespace ParkingPractice
         #endregion
 
         #region 隱式轉型
-        public static implicit operator DateTime(ParkingTime source)=>source.Value;
+        public static implicit operator DateTime(ParkingTime source) => source.Value;
         public static implicit operator ParkingTime(DateTime source) => new ParkingTime(source);
         #endregion
     }
