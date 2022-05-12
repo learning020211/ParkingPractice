3694265a1f5d6ece502d994798d059503b3d41ad
Steven Peng <steven020211@gmail.com>
2022-05-12 10:27:17 +0800

    add class ParkingTime

---
+++ ParkingPractice/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using ParkingPractice.Calculators;
+
+public static class Extensions
+{
+    public static DateTime SkipToMinutes(this DateTime dateTime) =>
+        dateTime.Date.AddMinutes((int)dateTime.TimeOfDay.TotalMinutes);
+    public static DateTime NextDateStart(this DateTime dateTime) =>
+        dateTime.Date.AddDays(1);
+    public static DateTime DateEnd(this DateTime dateTime) =>
+        dateTime.NextDateStart().AddMinutes(-1);
+    public static DateTime CompareAndTakeSmaller(this DateTime dateTime, DateTime dateTimeToCompare) =>
+        dateTime < dateTimeToCompare ? dateTime : dateTimeToCompare;
+    public static DayParkingRate GetRateByDate(this ParkingRate rate, DateTime dateTime) =>
+        dateTime.DayOfWeek switch
+        {
+            DayOfWeek.Saturday => rate.Holiday,
+            DayOfWeek.Sunday => rate.Holiday,
+            _ => rate.Weekday
+        };
+}
---
+++ ParkingPractice/ParkingTime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingPractice
+{
+    public class ParkingTime : IComparable<DateTime>
+    {
+        public DateTime Value { get; private set; }
+        public ParkingTime(DateTime parkingTime)
+        {
+            Value = parkingTime.SkipToMinutes();
+        }
+
+        public int CompareTo(DateTime other) => Value.CompareTo(other);
+
+        #region 運算子多載
+        public override bool Equals([NotNullWhen(true)] object? obj)=> base.Equals(obj);
+        public override int GetHashCode() => base.GetHashCode();
+
+        public static bool operator ==(ParkingTime parkingTime1, ParkingTime parkingTime2)=>parkingTime1.Value == parkingTime2.Value;
+        public static bool operator !=(ParkingTime parkingTime1, ParkingTime parkingTime2) => !(parkingTime1 == parkingTime2);
+
+        public static bool operator >(ParkingTime parkingTime1, ParkingTime parkingTime2) => parkingTime1.Value > parkingTime2.Value;
+        public static bool operator <(ParkingTime parkingTime1, ParkingTime parkingTime2) => parkingTime1.Value < parkingTime2.Value;
+
+        public static bool operator >=(ParkingTime parkingTime1, ParkingTime parkingTime2) => parkingTime1 > parkingTime2 || parkingTime1 == parkingTime2;
+        public static bool operator <=(ParkingTime parkingTime1, ParkingTime parkingTime2) => parkingTime1 < parkingTime2 || parkingTime1 == parkingTime2;
+        #endregion
+
+        #region 隱式轉型
+        public static implicit operator DateTime(ParkingTime source)=>source.Value;
+        public static implicit operator ParkingTime(DateTime source) => new ParkingTime(source);
+        #endregion
+    }
+}
